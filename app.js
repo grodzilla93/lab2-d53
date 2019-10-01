602bcd2fa21123228c279b54fb2d3d40204f36b8
wyatt <grodzilla93@gmail.com>
2019-10-01 14:43:21 -0700

    and i broke it

--- app.js
+++ app.js
@@ -3,12 +3,37 @@
 var user = prompt('Hello! who are you?');
 console.log('user'+ user);
 
-var hello = prompt('hey,' + user + 'ready to know me better? ');
+var hello = prompt('hey, ' + user + ' ready to know me better? ');
 
 hello = hello.toLowerCase();
 
-if(hello === 'yes') {
-  alert('gee thats a great attitude');
+if(hello === 'yes'|| 'y')
+ {
+  alert('gee' + user + 'thats a great attitude');
 } else {
-  alert('sucks.. have fun!');
-}
+  alert('sucks to be you ' + user + ' ...have fun!');
+
+var age = prompt('So ' + user + ' Am I 26?')
+
+  age = age.toLowerCase();
+
+  if(age === 'yes' || 'y')
+  //console.log( 'I am 26');
+   {
+    alert('Thats right! ' + user + ' Good guess');
+  } else {
+    alert('Nope ' + user + ' I am 26! Thank you very much');
+
+    hello = hello.toLowerCase();
+
+if(hello === 'yes') {
+  alert('gee' + user + 'thats a great attitude');
+} else {
+  alert('sucks to be you ' + user + ' ...have fun!');
+
+  hello = hello.toLowerCase();
+
+if(hello === 'yes') {
+  alert('gee' + user + 'thats a great attitude');
+} else {
+  alert('sucks to be you ' + user + ' ...have fun!');
