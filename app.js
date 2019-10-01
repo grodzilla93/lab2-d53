73f78b02050dfd495412c6d5154c8ccdb617e2fc
wyatt <grodzilla93@gmail.com>
2019-10-01 13:50:29 -0700

    closer to compleation

---
+++ app.js
@@ -0,0 +1,11 @@
+'use strict'
+
+var hello = prompt("hey, ready to know me better? ");
+
+hello = hello.toLowerCase();
+
+if(hello === "yes") {
+  alert("gee thats a great attitude");
+} else {
+  alert("sucks.. have fun!");
+}
