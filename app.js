6a3b37b6156b669157fedd439b0d3769645498ff
wyatt <grodzilla93@gmail.com>
2019-10-01 15:24:17 -0700

    4 questions down 1 to go

--- app.js
+++ app.js
@@ -7,33 +7,38 @@ var hello = prompt('hey, ' + user + ' ready to know me better? ');
 
 hello = hello.toLowerCase();
 
-if(hello === 'yes'|| 'y')
+if(hello === 'yes'|| hello === 'y')
  {
   alert('gee' + user + 'thats a great attitude');
 } else {
   alert('sucks to be you ' + user + ' ...have fun!');
-
+//console.log('the answer was yes, there was never a choice.')
 var age = prompt('So ' + user + ' Am I 26?')
 
   age = age.toLowerCase();
 
-  if(age === 'yes' || 'y')
-  //console.log( 'I am 26');
+  if(age === 'yes' || age === 'y')
+  //console.log( 'yes I am 26');
    {
     alert('Thats right! ' + user + ' Good guess');
   } else {
     alert('Nope ' + user + ' I am 26! Thank you very much');
 
-    hello = hello.toLowerCase();
+    var favec = prompt('Do you think hotpink is my favorite color?')
+    favec = favec.toLowerCase();
 
-if(hello === 'yes') {
-  alert('gee' + user + 'thats a great attitude');
+if(favec === 'no' || favec === 'n') {
+  alert('Thats right ' + user + ' Green is my favorite color');
 } else {
-  alert('sucks to be you ' + user + ' ...have fun!');
+  alert('Sorry ' + user + ' The only hot-pink I like is starbursts');
+  //console.log('No I do not like pink')
 
-  hello = hello.toLowerCase();
 
-if(hello === 'yes') {
-  alert('gee' + user + 'thats a great attitude');
+  var anime = prompt('Have I seen over 100 different Japanese anime?')
+  anime = anime.toLowerCase();
+
+if(anime === 'yes' anime === 'y') {
+  alert('Yeaaahhhhhh I will be honest ' + user + ' I would not reccomend even half of them);
 } else {
-  alert('sucks to be you ' + user + ' ...have fun!');
+  alert( user + ' I have seen more terrible anime then I care to confess');
+  //console.log('Yes I have seen over 100 anime')
